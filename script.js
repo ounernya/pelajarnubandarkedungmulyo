af30aca7a90bea52a6070869f2646fcb5f38ec25
ounernya <154839064+ounernya@users.noreply.github.com>
2023-12-26 03:49:02 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// toggle active //
+const navbarNav = document.querySelector('.navbar-nav');
+document.querySelector('#hamburger-menu').onclick = () => {
+    navbarNav.classList.toggle('active');
+};
+// klik luar slidebar hilang nav //
+const hamburger = document.querySelector('#hamburger-menu');
+document.addEventListener('click', function(e){
+    if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active');
+    }
+})
